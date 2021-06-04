678fb5140ad44eba9827ba9436e7f09dd1b8dad7
Paramjeet B <36541629+paramjeet01@users.noreply.github.com>
2021-06-04 18:58:07 +0530

    Update mysql-connection.php

--- php/mysql-connection.php
+++ php/mysql-connection.php
@@ -4,7 +4,7 @@ $host = '127.0.0.1';                               // HOSTNAME
 $user = getenv('DB_USER');                  // USERNAME FROM ENV
 $password = getenv('DB_PASS');              // PASSWORD FROM ENV
 
-$conn = new mysqli($host,$user,$password);      // CONNECTION TO DATABASE
+$conn = new mysqli($host,$user,$password,6033);      // CONNECTION TO DATABASE
 if($conn->connect_error){
  echo 'connection failed'. $conn->connect_error;
 }
